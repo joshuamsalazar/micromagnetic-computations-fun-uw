c74a08172d9818fc3bf16041e2194a9d05cc2037
Joshua <36059512+joshuamsalazar@users.noreply.github.com>
2021-10-11 13:58:44 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,3 +14,24 @@ hobby = st.selectbox("Hobbies: ",
  
 # print the selected hobby
 st.write("Your hobby is: ", hobby)
+
+periSampl = 1000
+
+class Parameters:
+    gamma = 2.2128e5
+    alpha = 1.0
+    K1 = 1.5 * 9100   
+    Js = 0.46
+    RAHE = 0.65 
+    d = (0.6+1.2+1.1) * 1e-9      
+    frequency = 0.1e9
+    currentd = float(sys.argv[1]) * 1e10
+    hbar = 1.054571e-34
+    e = 1.602176634e-19
+    mu0 = 4 * 3.1415927 * 1e-7
+    easy_axis = np.array([0,0,1])
+    p_axis = np.array([0,-1,0])
+    etadamp = 0.084    
+    etafield = 0.008   # etafield/etadamp=eta
+    eta = etafield/etadamp
+    hext = np.array([1.0 * K1/Js,0,0])
